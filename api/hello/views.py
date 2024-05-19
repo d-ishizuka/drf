b3abdf13a46162ea6bef968c11a5f89bca8043fc
d-ishizuka <vbstkaksa.0716@gmail.com>
2024-05-19 18:17:47 +0900

    use template_view

--- api/hello/views.py
+++ api/hello/views.py
@@ -1,33 +1,25 @@
+from typing import Any
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 from .forms import HelloForm
 
-# Create your views here.
-def index(request):
-    params = {
-        'title': 'Hello Django',
-        'message': 'your data:',
-        'form': HelloForm(),
-    }
+class HelloView(TemplateView):
+    
+    def __init__(self):
+        self.params = {
+            'title': 'Hello',
+            'message': 'your data:',
+            'form': HelloForm(),
+        }
 
-    if (request.method == 'POST'):
-        params['message'] = '名前: ' + request.POST['name'] + \
-            '<br>メール: ' + request.POST['mail'] + \
-            '<br>年齢: ' + request.POST['age']
-        params['form'] = HelloForm(request.POST)
-    return render(request, 'hello/index.html', params)
+    def get(self, request):
+        return render(request, 'hello/index.html', self.params)
 
-def next(request):
-    params = {
-        'title': 'Hello Next',
-        'msg': 'This is next page.',
-        'goto': 'index',
-    }
-    return render(request, 'hello/index.html', params)
-
-def form(request):
-    msg = request.POST['msg']
-    params = {
-        'title': 'Hello Form',
-        'msg': 'Hello ' + msg + '!!',
-    }
-    return render(request, 'hello/index.html', params)
+    def post(self, request):
+        msg = 'あなたは、<b>' + request.POST['name'] + \
+            '(' + request.POST['age'] + ') </b> さんです。<br>' + \
+            'メールアドレスは <b>' + request.POST['mail'] + '</b> ですね。'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'hello/index.html', self.params)
