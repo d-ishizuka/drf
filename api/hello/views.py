2723c893758526918b15d05fdc2a84c29517882b
d-ishizuka <vbstkaksa.0716@gmail.com>
2024-05-19 16:31:56 +0900

    use form object(forms.Form)

--- api/hello/views.py
+++ api/hello/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import HelloForm
 
 # Create your views here.
 def index(request):
     params = {
         'title': 'Hello Django',
-        'msg': 'This is a sample message.',
-        'goto': 'next',
+        'message': 'your data:',
+        'form': HelloForm(),
     }
+
+    if (request.method == 'POST'):
+        params['message'] = '名前: ' + request.POST['name'] + \
+            '<br>メール: ' + request.POST['mail'] + \
+            '<br>年齢: ' + request.POST['age']
+        params['form'] = HelloForm(request.POST)
     return render(request, 'hello/index.html', params)
 
 def next(request):
