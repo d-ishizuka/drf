8a58112eb605c671b00f138ada1a674e02c6d5c3
d-ishizuka <vbstkaksa.0716@gmail.com>
2024-05-19 22:56:09 +0900

    show all friends table records

--- api/hello/views.py
+++ api/hello/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .forms import HelloForm, SessionForm
+from .models import Friend
+
+
+def index(request):
+    data = Friend.objects.all()
+    params = {
+        'title': 'Hello',
+        'message': 'all friends',
+        'data': data,
+    }
+    return render(request, 'hello/index.html', params)
 
 class HelloView(TemplateView):
     
