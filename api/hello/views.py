489c9fb0d39932b07982bb9a78b37f4875c553d1
d-ishizuka <vbstkaksa.0716@gmail.com>
2024-05-19 14:58:48 +0900

    handle queryparams

--- api/hello/views.py
+++ api/hello/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Django!")
+    if 'msg' in request.GET:
+        msg = request.GET['msg']
+        result = 'you typed: ' + msg + '.'
+    else:
+        result = 'please send msg parameter!'
+    return HttpResponse(result)
